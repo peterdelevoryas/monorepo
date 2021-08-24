f01e96f5def8a57a04bb01d264c7dd56c516dc05
Peter Delevoryas <peterdelevoryas@gmail.com>
2021-08-23 21:49:39 -0700

    Stuff

---
+++ arm/main.c
@@ -0,0 +1,17 @@
+
+static void uart_init() {
+  
+}
+
+static void uart_puts(:q
+
+
+void main(void) {
+  uart_init();
+  uart_puts("hello world\n");
+
+  //for (;;) {
+  //  char c = uart_getc();
+  //  uart_send(c);
+  //}
+}
