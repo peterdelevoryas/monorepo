254b34818c81b5625906b3b49f3f993da45d2b13
Peter Delevoryas <pdel@fb.com>
2021-09-03 20:12:07 -0700

    arm: add shell prompt

--- arm/main.cc
+++ arm/main.cc
@@ -1,92 +1,139 @@
 #include <cstdint>
+#include <string_view>
 
-constexpr auto MMIO_BASE = 0x3F000000;
+using std::string_view;
+using std::size_t;
 
-#define REG(addr) ((volatile uint32_t*)(MMIO_BASE + addr))
+static auto AUX_IRQ             = (volatile uint32_t*)0x3F21'5000;
+static auto AUX_ENABLES         = (volatile uint32_t*)0x3F21'5004;
+static auto AUX_MU_IO_REG       = (volatile uint32_t*)0x3F21'5040;
+static auto AUX_MU_IER_REG      = (volatile uint32_t*)0x3F21'5044;
+static auto AUX_MU_IIR_REG      = (volatile uint32_t*)0x3F21'5048;
+static auto AUX_MU_LCR_REG      = (volatile uint32_t*)0x3F21'504C;
+static auto AUX_MU_MCR_REG      = (volatile uint32_t*)0x3F21'5050;
+static auto AUX_MU_LSR_REG      = (volatile uint32_t*)0x3F21'5054;
+static auto AUX_MU_MSR_REG      = (volatile uint32_t*)0x3F21'5058;
+static auto AUX_MU_SCRATCH      = (volatile uint32_t*)0x3F21'505C;
+static auto AUX_MU_CNTL_REG     = (volatile uint32_t*)0x3F21'5060;
+static auto AUX_MU_STAT_REG     = (volatile uint32_t*)0x3F21'5064;
+static auto AUX_MU_BAUD_REG     = (volatile uint32_t*)0x3F21'5068;
+static auto AUX_SPI0_CNTL0_REG  = (volatile uint32_t*)0x3F21'5080;
+static auto AUX_SPI0_CNTL1_REG  = (volatile uint32_t*)0x3F21'5084;
+static auto AUX_SPI0_STAT_REG   = (volatile uint32_t*)0x3F21'5088;
+static auto AUX_SPI0_IO_REG     = (volatile uint32_t*)0x3F21'5090;
+static auto AUX_SPI0_PEEK_REG   = (volatile uint32_t*)0x3F21'5094;
+static auto AUX_SPI1_CNTL0_REG  = (volatile uint32_t*)0x3F21'50C0;
+static auto AUX_SPI1_CNTL1_REG  = (volatile uint32_t*)0x3F21'50C4;
+static auto AUX_SPI1_STAT_REG   = (volatile uint32_t*)0x3F21'50C8;
+static auto AUX_SPI1_IO_REG     = (volatile uint32_t*)0x3F21'50D0;
+static auto AUX_SPI1_PEEK_REG   = (volatile uint32_t*)0x3F21'50D4;
+static auto GPFSEL0             = (volatile uint32_t*)0x3F20'0000;
+static auto GPFSEL1             = (volatile uint32_t*)0x3F20'0004;
+static auto GPFSEL2             = (volatile uint32_t*)0x3F20'0008;
+static auto GPFSEL3             = (volatile uint32_t*)0x3F20'000C;
+static auto GPFSEL4             = (volatile uint32_t*)0x3F20'0010;
+static auto GPFSEL5             = (volatile uint32_t*)0x3F20'0014;
+static auto GPPUD               = (volatile uint32_t*)0x3F20'0094;
+static auto GPPUDCLK0           = (volatile uint32_t*)0x3F20'0098;
+static auto GPPUDCLK1           = (volatile uint32_t*)0x3F20'009C;
 
-#define AUX_IRQ             REG(0x21'5000)
-#define AUX_ENABLES         REG(0x21'5004)
-#define AUX_MU_IO_REG       REG(0x21'5040)
-#define AUX_MU_IER_REG      REG(0x21'5044)
-#define AUX_MU_IIR_REG      REG(0x21'5048)
-#define AUX_MU_LCR_REG      REG(0x21'504C)
-#define AUX_MU_MCR_REG      REG(0x21'5050)
-#define AUX_MU_LSR_REG      REG(0x21'5054)
-#define AUX_MU_MSR_REG      REG(0x21'5058)
-#define AUX_MU_SCRATCH      REG(0x21'505C)
-#define AUX_MU_CNTL_REG     REG(0x21'5060)
-#define AUX_MU_STAT_REG     REG(0x21'5064)
-#define AUX_MU_BAUD_REG     REG(0x21'5068)
-#define AUX_SPI0_CNTL0_REG  REG(0x21'5080)
-#define AUX_SPI0_CNTL1_REG  REG(0x21'5084)
-#define AUX_SPI0_STAT_REG   REG(0x21'5088)
-#define AUX_SPI0_IO_REG     REG(0x21'5090)
-#define AUX_SPI0_PEEK_REG   REG(0x21'5094)
-#define AUX_SPI1_CNTL0_REG  REG(0x21'50C0)
-#define AUX_SPI1_CNTL1_REG  REG(0x21'50C4)
-#define AUX_SPI1_STAT_REG   REG(0x21'50C8)
-#define AUX_SPI1_IO_REG     REG(0x21'50D0)
-#define AUX_SPI1_PEEK_REG   REG(0x21'50D4)
+constexpr uint32_t AUX_MU_LSR_TX_READY = 0b0010'0000;
+constexpr uint32_t AUX_MU_LSR_RX_READY = 0b0000'0001;
 
-#define GPFSEL0   REG(0x20'0000)
-#define GPFSEL1   REG(0x20'0004)
-#define GPFSEL2   REG(0x20'0008)
-#define GPFSEL3   REG(0x20'000C)
-#define GPFSEL4   REG(0x20'0010)
-#define GPFSEL5   REG(0x20'0014)
-#define GPPUD     REG(0x20'0094)
-#define GPPUDCLK0 REG(0x20'0098)
-#define GPPUDCLK1 REG(0x20'009C)
-
-static int strlen(const char* s) {
-  int i = 0;
-  while (s[i]) {
-    i++;
-  }
-  return i;
+static void uart1_init() {
+    // Only enable UART1, disable SPI 1 and 2.
+    *AUX_IRQ = 1;
+    *AUX_ENABLES = 1;
+    // Disable UART1 interrupts.
+    *AUX_MU_IIR_REG = 0;
+    // Operate in 8-bit mode.
+    *AUX_MU_LCR_REG = 1;
+    // Set baud rate. 400_000_000 / (8 * (433 + 1)) = 115_200.
+    *AUX_MU_BAUD_REG = 433;
+    // Enable tx/rx.
+    *AUX_MU_CNTL_REG = 3;
 }
 
-void main() {
-  // Only enable UART1, disable SPI 1 and 2.
-  *AUX_IRQ = 1;
-  *AUX_ENABLES = 1;
-  // Disable UART1 interrupts.
-  *AUX_MU_IIR_REG = 0;
-  // Operate in 8-bit mode.
-  *AUX_MU_LCR_REG = 1;
-  // Set baud rate. 400_000_000 / (8 * (433 + 1)) = 115_200.
-  *AUX_MU_BAUD_REG = 433;
-  // Enable tx/rx.
-  *AUX_MU_CNTL_REG = 3;
-
-  auto r = *GPFSEL1;
-  // Clear previous GPIO 14 and 15 function settings.
-  r &= ~(0b111 << 12);
-  r &= ~(0b111 << 15);
-  // Set GPIO 14 and 15 to TX and RX UART1.
-  r |= 0b010 << 12;
-  r |= 0b010 << 15;
-  *GPFSEL1 = r;
-  *GPPUD = 0;
-  for (int i = 0; i < 150; i++) {
-    asm volatile("nop");
-  }
-  *GPPUDCLK0 = (1 << 14) | (1 << 15);
-  for (int i = 0; i < 150; i++) {
-    asm volatile("nop");
-  }
-  *GPPUDCLK0 = 0;
-
-  auto message = "hello world";
-  for (int i = 0; i < strlen(message); i++) {
-    auto transmitter_empty = *AUX_MU_LSR_REG & 0b100000;
-    while (!transmitter_empty) {
-      asm volatile("nop");
+static void uart1_init_gpio() {
+    auto r = *GPFSEL1;
+    // Clear previous GPIO 14 and 15 function settings.
+    r &= ~(0b111 << 12);
+    r &= ~(0b111 << 15);
+    // Set GPIO 14 and 15 to TX and RX UART1.
+    r |= 0b010 << 12;
+    r |= 0b010 << 15;
+    *GPFSEL1 = r;
+    *GPPUD = 0;
+    for (int i = 0; i < 150; i++) {
+        asm volatile("nop");
     }
-    *AUX_MU_IO_REG = message[i];
-  }
+    *GPPUDCLK0 = (1 << 14) | (1 << 15);
+    for (int i = 0; i < 150; i++) {
+        asm volatile("nop");
+    }
+    *GPPUDCLK0 = 0;
+}
 
-  while (1) {
-    asm volatile("nop");
-  }
+static char uart1_getc() {
+    for (;;) {
+        auto rx_ready = *AUX_MU_LSR_REG & AUX_MU_LSR_RX_READY;
+        if (rx_ready) {
+            break;
+        }
+    }
+    auto c = static_cast<char>(*AUX_MU_IO_REG);
+    return c;
+}
+
+static void uart1_putc(char c) {
+    for (;;) {
+        auto tx_ready = *AUX_MU_LSR_REG & AUX_MU_LSR_TX_READY;
+        if (tx_ready) {
+            break;
+        }
+    }
+    *AUX_MU_IO_REG = static_cast<uint32_t>(c);
+}
+
+static void uart1_puts(string_view s) {
+    auto n = s.size();
+    for (size_t i = 0; i < n; i++) {
+        uart1_putc(s[i]);
+    }
+}
+
+constexpr auto SPLASH_TEXT =
+"\n"
+"       Operating System\n"
+"\n";
+
+constexpr auto PROMPT = "> ";
+
+extern "C"
+int main() {
+    uart1_init();
+    uart1_puts(SPLASH_TEXT);
+
+    while (1) {
+        uart1_puts(PROMPT);
+        char line[256];
+        int i = 0;
+        for (; i < sizeof(line) - 1; i++) {
+            auto c = uart1_getc();
+            if (c == '\r') {
+                break;
+            }
+            line[i] = c;
+            uart1_putc(c);
+        }
+        line[i] = '\0';
+        uart1_putc('\n');
+    }
+}
+
+extern "C"
+size_t strlen(const char* s) {
+    size_t i = 0;
+    for (; s[i]; i++);
+    return i;
 }
