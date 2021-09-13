9ee15382cb59696e2a9ec7f62ad8f53306c4222b
Peter Delevoryas <pdel@fb.com>
2021-09-13 06:50:05 -0700

    software-renderer: Rewrite from helsinki tutorial

--- software-renderer/main.cpp
+++ software-renderer/main.cpp
@@ -1,109 +1,172 @@
 #include <cstdio>
 #include <cstdlib>
 #include <cstdint>
-#include <cstring>
 #include <cassert>
-#include <sys/mman.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <cmath>
+#include <utility>
+#include <string>
+#include <string_view>
 
-#define swap(a, b)  \
-do {                \
-  auto tmp = a;     \
-  a = b;            \
-  b = tmp;          \
-} while (0)         \
+template<typename T>
+struct Vec {
+    T *ptr;
+    uint32_t len, cap;
 
-struct Image {
-  struct Pixel {
-    uint8_t b;
-    uint8_t g;
-    uint8_t r;
-  };
-
-  struct Point {
-    int x;
-    int y;
-  };
-
-  Pixel* data;
-  int width;
-  int height;
-
-  static Image zeros(int width, int height) {
-    auto size = sizeof(Pixel) * width * height;
-    auto data = static_cast<Pixel*>(malloc(size));
-    memset(data, 0, size);
-    return {data, width, height};
-  }
-
-  static constexpr uint8_t UNCOMPRESSED_TRUE_COLOR_IMAGE = 2;
-  static constexpr uint8_t ALPHA_CHANNEL_DEPTH = 0;
-  static constexpr uint8_t LOWER_LEFT_HAND_ORIGIN = 0;
-
-  void write_tga_file(const char* path) {
-    uint8_t header[18] = {};
-    header[2] = UNCOMPRESSED_TRUE_COLOR_IMAGE;
-    header[12] = (width & 0x0000'00FF);
-    header[13] = (width & 0x0000'FF00) >> 8;
-    header[14] = (height & 0x0000'00FF);
-    header[15] = (height & 0x0000'FF00) >> 8;
-    header[16] = sizeof(Pixel) * 8; // Number of bits per pixel.
-    header[17] = ALPHA_CHANNEL_DEPTH | (LOWER_LEFT_HAND_ORIGIN << 5);
-
-    auto file = fopen(path, "w");
-    fwrite(header, sizeof(header), 1, file);
-    fwrite(data, sizeof(Pixel), width * height, file);
-    fclose(file);
-  }
-
-  Pixel& at(int x, int y) {
-    return data[y * width + x];
-  }
-
-  void draw_line(Point a, Point b, Pixel c) {
-    //printf("draw_line (%u, %u) (%u, %u) #%02x%02x%02x\n", a.x, a.y, b.x, b.y, c.r, c.g, c.b);
-    if (a.x > b.x) {
-      swap(a, b);
+    void push(T &&v) {
+        if (len >= cap) {
+            cap = cap ? cap * 3 / 2 : 8;
+            ptr = realloc(ptr, sizeof(T) * cap);
+            assert(ptr);
+        }
+        ptr[len++] = std::move(v);
     }
 
-    bool steep = false;
-    int dx = b.x - a.x;
-    int dy = b.y - a.y;
-    if (dy > dx) {
-      swap(a.x, a.y);
-      swap(b.x, b.y);
-      steep = true;
+    const T &operator[](uint32_t i) const {
+        assert(i < len);
+        return ptr[i];
     }
 
-    int derr = dy * 2;
-    int err = 0;
-
-    for (int x = a.x, y = a.y; x <= b.x; x++) {
-      int x_ = steep ? y : x;
-      int y_ = steep ? x : y;
-      data[x_ * width + y_] = c;
-      err += derr;
-      if (err < dx) {
-        continue;
-      }
-      y +=
-      err -= dx * 2;
+    T &operator[](uint32_t i) {
+        assert(i < len);
+        return ptr[i];
     }
-  }
 
-  void free() {
-    ::free(data);
-  }
+    const T *begin() const {
+        return ptr;
+    }
+
+    const T *end() const {
+        return ptr + len;
+    }
+
+    T *begin() {
+        return ptr;
+    }
+
+    T *end() {
+        return ptr + len;
+    }
 };
 
-constexpr int N = 1'000'000;
-constexpr int M = 100;
+struct Obj {
+    struct Vertex {
+        float x, y, z;
+    };
+    struct Face {
+        uint16_t vertex_index[3];
+        uint16_t texcoord_index[3];
+        uint16_t normal_index[3];
+    };
+    struct Group {
+        std::string_view name;
+        Vec<Face> faces;
+    };
+    std::string string_pool;
+    Vec<Group> groups;
 
-int main(int argc, char** argv) {
-  auto image = Image::zeros(M, M);
-  image.draw_line({0, 0}, {99, 99}, {0, 0, 0xFF});
-  image.write_tga_file("out.tga");
-  image.free();
+    struct Parser {
+        FILE *file;
+        Obj &obj;
+        uint32_t line_no;
+
+        void parse_file() {
+
+        }
+    };
+
+    static Obj read_file(const char *path) {
+        auto file = fopen(path, "r");
+        if (!file) {
+            perror("Unable to open obj file");
+            return {};
+        }
+        auto obj = Obj{};
+        auto parser = Parser{file, obj};
+        parser.parse_file();
+        fclose(file);
+        return obj;
+    }
+};
+
+struct Image {
+    struct Pixel {
+        uint8_t b, g, r;
+    };
+    Pixel *data;
+    int width, height;
+
+    static Image init(int width, int height) {
+        auto data = static_cast<Pixel*>(calloc(sizeof(Pixel), width * height));
+        return {data, width, height};
+    }
+
+    void free() {
+        ::free(data);
+    }
+
+    void write_tga_file(const char *path) {
+        uint8_t header[18] = {};
+        // Uncompressed true color image.
+        header[2] = 2;
+        // Width, little-endian byte-order.
+        header[12] = width & 0xFF;
+        header[13] = (width & 0xFF00) >> 8;
+        // Height, little-endian byte-order.
+        header[14] = height & 0xFF;
+        header[15] = (height & 0xFF00) >> 8;
+        // Bits-per-pixel.
+        header[16] = sizeof(Pixel) * 8;
+
+        auto file = fopen(path, "w");
+        fwrite(header, sizeof(header), 1, file);
+        fwrite(data, sizeof(Pixel), width * height, file);
+        fclose(file);
+    }
+
+    void draw_line(int x0, int y0, int x1, int y1) {
+        bool steep = false;
+        if (std::abs(y1 - y0) > std::abs(x1 - x0)) {
+            std::swap(x0, y0);
+            std::swap(x1, y1);
+            steep = true;
+        }
+        if (x1 < x0) {
+            std::swap(x0, x1);
+            std::swap(y0, y1);
+        }
+        int dx = x1 - x0;
+        int dy = y1 - y0;
+        int de = std::abs(dy);
+        int e = 0;
+        int y = y0;
+        int y_step = y0 < y1 ? 1 : -1;
+        for (int x = x0; x <= x1; x++) {
+            // printf("(%d, %d) e=%d\n", x, y, e);
+            if (steep) {
+                data[x * width + y] = {0, 0, 0xFF};
+            } else {
+                data[y * width + x] = {0, 0, 0xFF};
+            }
+            e += de;
+            if (2 * e >= dx) {
+                y += y_step;
+                e -= dx;
+            }
+        }
+    }
+};
+
+int main() {
+    auto image = Image::init(800, 600);
+
+    for (int x = 0; x < 800; x += 100) {
+        image.draw_line(x, 0, x, 599);
+    }
+    for (int y = 0; y < 600; y += 100) {
+        image.draw_line(0, y, 799, y);
+    }
+    auto head = Obj::read_file("head.obj");
+    (void)head;
+
+    image.write_tga_file("out.tga");
+    image.free();
 }
