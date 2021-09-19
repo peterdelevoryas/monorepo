4f2198c832c2bd7ef7957d5040bf8f5ca28e4a75
Peter Delevoryas <pdel@fb.com>
2021-09-19 15:53:24 -0700

    software-renderer: added head drawing

--- software-renderer/main.cpp
+++ software-renderer/main.cpp
@@ -5,16 +5,21 @@
 #include <utility>
 #include <string>
 #include <string_view>
+#include <sys/mman.h>
 
 template<typename T>
 struct Vec {
     T *ptr;
     uint32_t len, cap;
 
-    void push(T &&v) {
+    ~Vec() {
+        free(ptr);
+    }
+
+    void push(T v) {
         if (len >= cap) {
             cap = cap ? cap * 3 / 2 : 8;
-            ptr = realloc(ptr, sizeof(T) * cap);
+            ptr = static_cast<T*>(realloc(ptr, sizeof(T) * cap));
             assert(ptr);
         }
         ptr[len++] = std::move(v);
@@ -44,46 +49,6 @@ struct Vec {
 
     T *end() {
         return ptr + len;
-    }
-};
-
-struct Obj {
-    struct Vertex {
-        float x, y, z;
-    };
-    struct Face {
-        uint16_t vertex_index[3];
-        uint16_t texcoord_index[3];
-        uint16_t normal_index[3];
-    };
-    struct Group {
-        std::string_view name;
-        Vec<Face> faces;
-    };
-    std::string string_pool;
-    Vec<Group> groups;
-
-    struct Parser {
-        FILE *file;
-        Obj &obj;
-        uint32_t line_no;
-
-        void parse_file() {
-
-        }
-    };
-
-    static Obj read_file(const char *path) {
-        auto file = fopen(path, "r");
-        if (!file) {
-            perror("Unable to open obj file");
-            return {};
-        }
-        auto obj = Obj{};
-        auto parser = Parser{file, obj};
-        parser.parse_file();
-        fclose(file);
-        return obj;
     }
 };
 
@@ -140,12 +105,11 @@ struct Image {
         int y = y0;
         int y_step = y0 < y1 ? 1 : -1;
         for (int x = x0; x <= x1; x++) {
-            // printf("(%d, %d) e=%d\n", x, y, e);
-            if (steep) {
-                data[x * width + y] = {0, 0, 0xFF};
-            } else {
-                data[y * width + x] = {0, 0, 0xFF};
+            auto i = steep ? x * width + y : y * width + x;
+            if (0 < i && i < width * height) {
+                data[i] = {0, 0, 0xFF};
             }
+            // printf("(%d, %d) e=%d\n", x, y, e);
             e += de;
             if (2 * e >= dx) {
                 y += y_step;
@@ -154,6 +118,73 @@ struct Image {
         }
     }
 };
+
+struct Obj {
+    struct Vertex {
+        float x;
+        float y;
+        float z;
+    };
+    struct Triangle {
+        uint16_t vertex_index[3];
+    };
+
+    Vec<Vertex> vertices;
+    Vec<Triangle> triangles;
+};
+
+static Obj::Vertex parse_vertex(FILE *f) {
+    auto v = Obj::Vertex{};
+    if (fscanf(f, "%f %f %f\n", &v.x, &v.y, &v.z) != 3) {
+        printf("Error parsing vertex\n");
+        exit(1);
+    }
+    return v;
+}
+
+static Obj::Triangle parse_triangle(FILE *f) {
+    auto t = Obj::Triangle{};
+    uint16_t unused = 0;
+    if (fscanf(f, " %hu/%hu/%hu %hu/%hu/%hu %hu/%hu/%hu\n",
+               &t.vertex_index[0], &unused, &unused,
+               &t.vertex_index[1], &unused, &unused,
+               &t.vertex_index[2], &unused, &unused) != 9) {
+        printf("Error parsing face\n");
+        exit(1);
+    }
+    return t;
+}
+
+Obj read_obj_file(const char *path) {
+    auto f = fopen(path, "r");
+    auto obj = Obj{};
+
+    while (!feof(f)) {
+        int c = getc(f);
+        switch (c) {
+            case 'v': {
+                c = getc(f);
+                if (c != ' ') {
+                    break;
+                }
+                auto v = parse_vertex(f);
+                obj.vertices.push(v);
+                break;
+            }
+            case 'f': {
+                auto t = parse_triangle(f);
+                obj.triangles.push(t);
+                break;
+            }
+            default:
+                while (c != '\n') c = getc(f);
+                break;
+        }
+    }
+
+    fclose(f);
+    return obj;
+}
 
 int main() {
     auto image = Image::init(800, 600);
@@ -164,8 +195,19 @@ int main() {
     for (int y = 0; y < 600; y += 100) {
         image.draw_line(0, y, 799, y);
     }
-    auto head = Obj::read_file("head.obj");
-    (void)head;
+
+    auto obj = read_obj_file("head.obj");
+    for (const auto& t : obj.triangles) {
+        for (int i = 0; i < 3; i++) {
+            auto a = obj.vertices[t.vertex_index[i] - 1];
+            auto b = obj.vertices[t.vertex_index[(i + 1) % 3] - 1];
+            int x0 = (a.x + 1.0f) * (image.width / 2);
+            int y0 = (a.y + 1.0f) * (image.height / 2);
+            int x1 = (b.x + 1.0f) * (image.width / 2);
+            int y1 = (b.y + 1.0f) * (image.height / 2);
+            image.draw_line(x0, y0, x1, y1);
+        }
+    }
 
     image.write_tga_file("out.tga");
     image.free();
