a5e88e26e308fe84710d139c5ca3745661c7478d
Peter Delevoryas <pdel@fb.com>
2021-10-31 17:24:11 -0700

    d

--- swr/main.c
+++
@@ -1,9 +0,0 @@
-#include "obj.h"
-#include "tga.h"
-
-int main(int argc, char** argv) {
-    let obj = obj_load("head.obj");
-    let tga = tga_init(1000, 1000);
-    obj_free(&obj);
-    tga_free(&tga);
-}
